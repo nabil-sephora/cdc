9ce4289ebdc2efc2d4ac11b9b232b372c3117280
Nabil M Khan <nabil.khan@sephora.com>
2025-02-16 10:12:30 -0800

    adding logs.sh and Bug fix on connector

---
+++ logs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Start the topology as defined in https://debezium.io/documentation/reference/stable/tutorial.html
+export DEBEZIUM_VERSION=2.1
+docker-compose -f docker-compose-mysql.yaml logs -f "$@"
